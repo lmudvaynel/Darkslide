4601b8e0d071829bc89b9fb158e7c912880034f7
I.Simutin <i.simutin.gmail.com>
2014-01-23 10:55:32 +0200

    Add form for admin panel to create new pages with seo fields

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -1,4 +1,25 @@
 ActiveAdmin.register Page do
 
+	controller do
+    def new
+      @page = Page.new
+      @page.build_seo   # без этой строки не появляется форма для seo параметров
+    end
+    def edit
+      @page = Page.find(params[:id])
+      if @page.seo_id.nil?
+        @page.build_seo
+      end
+    end
+  end
+
+	index do
+    column :id
+    column :name
+    column :slug
+    
+    default_actions
+  end
   
+  form partial: 'form'
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,7 @@
 class Page < ActiveRecord::Base
-  attr_accessible :content, :name, :slug
+  attr_accessible :content, :name, :slug, :seo_id, :seo_attributes
+  
+  belongs_to :seo
+
+  accepts_nested_attributes_for :seo, :allow_destroy => true, :reject_if => :all_blank
 end
