b00ade97c8f254d6f58f404b5a3dec553d5c3243
I.Simutin <i.simutin.gmail.com>
2014-01-23 13:31:52 +0200

    Add SEO meta tags helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	
+	def seo_meta_tags
+    if @page && @page.seo
+      "<meta name='description' content='#{@page.seo.description}' />\n"+
+      "<meta name='keywords' content='#{@page.seo.keywords}' />\n"+
+      "<title>#{@page.seo.title}</title>"
+    end
+  end
 end
