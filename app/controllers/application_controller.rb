f31f1b4403677580ea5a58cebe1ddf4b3e87cf63
simutin <simutin@balticit.ru>
2014-03-07 12:29:26 +0200

    Add errors pages views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,30 @@ class ApplicationController < ActionController::Base
   def set_admin_locale
     I18n.locale = params[:locale] || :ru
   end
+
+  private
+
+	  def render_403(exception)
+	    respond_to do |format|
+	      format.html { render template: 'errors/error_403', layout: 'layouts/application', status: 403 }
+	      format.all { render nothing: true, status: 403 }
+	    end
+	  end
+
+	  def render_404(exception)
+	    respond_to do |format|
+	      format.html { render 'errors/error_404', status: 404 }
+	      format.all { render nothing: true, status: 404 }
+	    end
+	  end
+
+	  def render_500(exception)
+	    ExceptionNotifier.notify_exception(exception,
+	                                       env: request.env,
+	                                       data: {message: 'an error happened'})
+	    respond_to do |format|
+	      format.html { render template: 'errors/error_500', layout: 'layouts/application', status: 500 }
+	      format.all { render nothing: true, status: 500}
+	    end
+	  end
 end
