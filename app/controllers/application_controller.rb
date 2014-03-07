f59d865221aff5bb78541399b3717a48ce88ff49
simutin <simutin@balticit.ru>
2014-03-07 13:12:12 +0200

    Hot fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,18 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def set_admin_locale
-    I18n.locale = params[:locale] || :ru
-  end
-  
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, with: :render_500
-    rescue_from ActionController::RoutingError, with: :render_404
-    rescue_from ActionController::UnknownController, with: :render_404
-    rescue_from ::AbstractController::ActionNotFound, with: :render_404
-    rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  end
+	  def set_admin_locale
+	    I18n.locale = params[:locale] || :ru
+	  end
+	  
+	  unless Rails.application.config.consider_all_requests_local
+	    rescue_from Exception, with: :render_500
+	    rescue_from ActionController::RoutingError, with: :render_404
+	    rescue_from ActionController::UnknownController, with: :render_404
+	    rescue_from ::AbstractController::ActionNotFound, with: :render_404
+	    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+	  end
 
-  private
 
 	  def render_403(exception)
 	    respond_to do |format|
