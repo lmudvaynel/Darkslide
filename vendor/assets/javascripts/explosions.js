b83d2dc8778eca4981ca0fe370f587b3e8132525
simutin <simutin@balticit.ru>
2014-03-27 16:32:11 +0200

    Final update drawing script

--- vendor/assets/javascripts/explosions.js
+++ vendor/assets/javascripts/explosions.js
@@ -1,15 +1,16 @@
+$(document).ready(function() {
 visuals = {
 
 	drawing: function(mx, my, h, w, timeScale) {
 		for (k = 0; k < 7; k++) {
 			svg.append("svg:circle")
 				.attr("cx",mx).attr("cy",my).attr("r",2)
-				.style("stroke","#00FFFF").style("fill","#00FFFF")
+				.style("stroke","#000000").style("fill","#000000")
 				.transition().delay(5000).duration(timeScale*800)
 					.attr("cx",mx+Math.floor(Math.random()*200)-100).attr("cy",my+Math.floor(Math.random()*200)-100)
 					.style("stroke",colors(++ci)).style("fill",colors(++ci)).style("stroke-opacity",1e-6).style("fill-opacity",1e-6).remove();
 		}
-	},
+	}
 };
 
 var svg = d3.select("#svgContainer").append("svg:svg").style("pointer-events", "all");
@@ -18,28 +19,11 @@ var ci=0;
 var debug = false;
 function log(msg) {if (debug) {console.log(msg);}}
 
-
-stringConverter = {};
-
-function toggleExporter() {
-	$('#exported, .button-export, .button-record').toggle();
-}
-
-function exportToTextarea() {
-	var data = recorder.entriesToJSON();
-	$('#exported').val(data);
-}
-function importFromTextarea () {
-	var data = $('#exported').val();
-	recorder.entriesFromJSON(data);
-}
-
 function mouseHandler(visualName) {
 	return function() {
 		var m = d3.mouse(svg[0][0]);
 		var w = window.innerWidth, h = window.innerHeight;
 		var fmx = m[0]/w, fmy = m[1]/h;
-		if (window.recorder) {recorder.record([visualName, fmx, fmy]);}
 		return doVisual(visualName, fmx, fmy);
 	};
 }
@@ -56,21 +40,6 @@ function setEventHandler(visualName, eventName) {
 	svg.on(eventName, mouseHandler(visualName));
 }
 
-function keystrokes(event) {
-	if ($('textarea:focus, input:focus, select:focus').length>0) {return;}
-	var k = event.charCode;
-	s = String.fromCharCode(k);
-	var $e = $(keyAliases[s]);
-	if ($e.is('option')) {
-		$e.parent().val($e.val());
-		$e.change();
-	}
-	else if ($e.attr('disabled') === undefined) {
-		$e.click();
-	}
-}
-$(document).keypress(keystrokes);
 
-$(document).ready(function() {
 	setEventHandler('drawing', 'mousemove');
 });
