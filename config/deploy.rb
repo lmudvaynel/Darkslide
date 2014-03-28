1d51493aee751bf9cb7b6371a0b73219ad7408be
simutin <simutin@balticit.ru>
2014-03-28 11:20:01 +0200

    Config production deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :stages, %w(production staging)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 
-set :application, "dslide" #write app name there
+set :application, "daslide" #write app name there
 
 set :scm, :git
 set :repository,  "git@github.com:simutin/Darkslide.git" #place your repo there
--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,4 +1,4 @@
-app_dir = "/var/www/apps/dslide_production"
+app_dir = "/var/www/apps/daslide_production"
 
 # Set unicorn options
 worker_processes 4
